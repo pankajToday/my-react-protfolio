46d6710184fa1ff1e9411661d2a34c377a8a2415
pankaj-kumar <p1608088@gmail.com>
2025-02-12 04:15:16 +0530

    new add

--- src/components/landing/header.js
+++ src/components/landing/header.js
@@ -4,7 +4,7 @@ export default function lendingHeader() {
   return (
     <header class="masthead">
     <div class="container">
-        <div class="masthead-subheading">Thanks your visiting!</div>
+        <div class="masthead-subheading">Thanks for your visiting!</div>
         <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
         {/* <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a> */}
     </div>

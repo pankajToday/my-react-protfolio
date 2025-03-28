f90b6c0336d75b85e4b506b710f8b1ae14ac39c8
pankaj-kumar <p1608088@gmail.com>
2025-03-28 22:12:54 +0530

    bg change

--- src/components/landing/header.js
+++ src/components/landing/header.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function lendingHeader() {
   return (
     <header class="masthead">
-    <div class="container">
+    <div class="container" style={{ 
+         backgroundColor: 'darkmagenta',
+         opacity: 0.8,
+         padding: '20px' ,
+         borderRadius: '80% 80%',
+        }}>
         <div class="masthead-subheading">Thanks for your visiting!</div>
         <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
         {/* <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a> */}
